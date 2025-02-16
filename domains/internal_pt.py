fcf29d669af9dad697719aade2783f05f577b06e
3113v3n <sydneyreezy+github@gmail.com>
2025-02-16 10:21:35 +0300

    changed display text after IP enumeration

--- domains/internal_pt.py
+++ domains/internal_pt.py
@@ -11,11 +11,12 @@ class InternalAssessment(Bcolors):
         self.mode = "SCAN"
         self.network_manager = network
 
-    def initialize_variables(self,mode, output_file):
+    def initialize_variables(self, mode, output_file):
         # Sets user provided values
         self.mode = mode
         if mode == "scan":
-            self.output_file = self.network_manager.generate_unique_name(output_file, "csv")
+            self.output_file = self.network_manager.generate_unique_name(
+                output_file, "csv")
         else:
 
             self.output_file = output_file
@@ -24,13 +25,14 @@ class InternalAssessment(Bcolors):
         """Enumerates all possible hosts on a network using ICMP protocol
         In order to increase your attack surface
         """
-        self.network_manager.get_live_ips(mode=self.mode, output=self.output_file)
+        self.network_manager.get_live_ips(
+            mode=self.mode, output=self.output_file)
         paths = self.network_manager.get_file_paths()
         live = f"{self.ENDC}{self.BOLD}{self.OKGREEN}{paths['live_hosts']}"
         unresponsive = f"{self.ENDC}{self.BOLD}{self.WARNING}{paths['unresponsive_hosts']}"
 
         print(
-            f"\n{self.OKCYAN}[+] Your Live Hosts are located here: {live}{self.ENDC}\n"
+            f"\n{self.OKCYAN}[+] Discovered hosts are saved here:\n {live}{self.ENDC}\n"
             f"\n{self.HEADER}[!] Your Unresponsive Hosts are located here: {unresponsive}{self.ENDC}\n\n"
         )
 
